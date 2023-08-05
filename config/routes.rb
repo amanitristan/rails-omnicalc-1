52859e8aa1333e0b4de8193d938f2b6f155a20d7
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-05 22:00:16 +0000

    Completed routes, controller.rb setup, form erb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get("/", { controller: "omnicalc1", action: "home" })
+
+  get("/", { controller: "omnicalc1", action: "square" })
+
+  get("/", { controller: "omnicalc1", action: "square_root" })
+
+  get("/", { controller: "omnicalc1", action: "payment" })
+
+  get("/", { controller: "omnicalc1", action: "random" })
 end
